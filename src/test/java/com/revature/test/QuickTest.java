3fada93bc1f969818ccd5f602607bb01ee5463af
Dat Nguyen <datn11617@gmail.com>
2018-11-15 17:04:43 -0500

    update QuickTest.java

--- src/test/java/com/revature/test/QuickTest.java
+++ src/test/java/com/revature/test/QuickTest.java
@@ -9,14 +9,14 @@ import com.revature.dao.EmployeeDaoImpl;
 import com.revature.model.Employee;
 
 public class QuickTest {
-	private static final QuickTest quickTest = new QuickTest();
+//	private static final QuickTest quickTest = new QuickTest();
 	
-	public ExpectedException expectedException = ExpectedException.none();
+//	public ExpectedException expectedException = ExpectedException.none();
 	
-	@Test
-	public void testNewEmployeeCreation() {
-		Employee e = new Employee("doctor", "who", "absoluteBrandNewUser", "123", "dwho@winning.com", "Gallifrey", "true");
+//	@Test
+//	public void testNewEmployeeCreation() {
+//		Employee e = new Employee("doctor", "who", "absoluteBrandNewUser", "123", "dwho@winning.com", "Gallifrey", "true");
 		
-		assertFalse(EmployeeDaoImpl.getEmployeeDaoImplInstance().addNewEmployee(e));
-	}
+//		assertFalse(EmployeeDaoImpl.getEmployeeDaoImplInstance().addNewEmployee(e));
+//	}
 }
